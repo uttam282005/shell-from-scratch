fcb94446cb8d1552074226aed742218cf7608573
uttam282005 <uttam282005@gmail.com>
2025-06-04 22:42:22 +0530

    feat: implemented REPL

--- src/main.c
+++ src/main.c
@@ -7,15 +7,16 @@ int main(int argc, char *argv[]) {
   setbuf(stdout, NULL);
 
   // Uncomment this block to pass the first stage
-  printf("$ ");
 
   // Wait for user input
   char input[100];
-  fgets(input, 100, stdin);
 
-  input[strlen(input) - 1] = '\0';
-
-  printf("%s: command not found\n", input);
+  while (1) {
+    printf("$ ");
+    fgets(input, 100, stdin);
+    input[strlen(input) - 1] = '\0';
+    printf("%s: command not found\n", input);
+  }
 
   return 0;
 }
