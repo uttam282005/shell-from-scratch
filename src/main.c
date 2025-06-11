0503de78c14af511b123fd6548081d57c49327c4
uttam282005 <uttam282005@gmail.com>
2025-06-12 00:31:19 +0530

    feat: implemented type command

--- src/main.c
+++ src/main.c
@@ -14,9 +14,11 @@ int main(int argc, char *argv[]) {
 
   while (1) {
     bool is_valid_command = false;
+
     printf("$ ");
     fgets(input, 100, stdin);
-    input[strlen(input) - 1] = '\0';
+    input[strlen(input) - 1] = ' ';
+    input[strlen(input)] = '\0';
 
     if (strcmp(input, "exit 0") == 0)
       break;
@@ -32,26 +34,60 @@ int main(int argc, char *argv[]) {
     char word[100];
 
     int character = 0;
-    bool is_echo = false;
 
-    for (int c = first_char; input[c] != '\0'; c++) {
+    bool is_echo = false;
+    bool is_type = false;
+
+    char *command;
+    int c = 0;
+
+    for (c = first_char; input[c] != '\0'; c++) {
       if (input[c] == ' ') {
         word[character] = '\0';
-        if (is_echo) {
-          printf("%s ", word);
-        }
-        if (strcmp(word, "echo") == 0) {
-          is_echo = true;
-          is_valid_command = true;
-        }
-        character = 0;
+        command = word;
+        break;
       } else
         word[character++] = input[c];
     }
 
-    word[character] = '\0';
+    c++;
+
+    if (strcmp(command, "echo") == 0) {
+      is_echo = true;
+      is_valid_command = true;
+    }
+
+    if (strcmp(command, "type") == 0) {
+      is_type = true;
+      is_valid_command = true;
+    }
+
+    character = 0;
+    while (input[c] != '\0') {
+      if (input[c] == ' ') {
+
+        word[character] = '\0';
+
+        if (is_echo) {
+          printf("%s ", word);
+        }
+
+        if (is_type) {
+          if (strcmp(word, "echo") == 0 || strcmp(word, "type") == 0)
+            printf("%s is a shell builtin\n", word);
+          else
+            printf("%s: not found\n", word);
+        }
+
+        character = 0;
+
+      } else
+        word[character++] = input[c];
+      c++;
+    }
+
     if (is_echo)
-      printf("%s\n", word);
+      printf("%s\n", "");
 
     if (!is_valid_command)
       printf("%s: command not found\n", input);
